ca2d21aa9f2a92a6cdb6eb9962329dd1674d60d9
Nannis96 <danni.mbb.96@gamil.com>
2026-03-27 08:50:34 -0600

    migrate

--- prisma/migrations/20260311202110_simplify_post_model/migration.sql
+++ prisma/migrations/20260311202110_simplify_post_model/migration.sql
@@ -17,22 +17,22 @@
   - Added the required column `title` to the `Post` table without a default value. This is not possible if the table is not empty.
 
 */
--- -- AlterTable
--- ALTER TABLE "Post" DROP COLUMN "authorBioEn",
--- DROP COLUMN "authorBioEs",
--- DROP COLUMN "contentEn",
--- DROP COLUMN "contentEs",
--- DROP COLUMN "focusKeywordEn",
--- DROP COLUMN "focusKeywordEs",
--- DROP COLUMN "seoDescEn",
--- DROP COLUMN "seoDescEs",
--- DROP COLUMN "seoTitleEn",
--- DROP COLUMN "seoTitleEs",
--- DROP COLUMN "titleEn",
--- DROP COLUMN "titleEs",
--- ADD COLUMN     "authorBio" TEXT,
--- ADD COLUMN     "content" TEXT NOT NULL,
--- ADD COLUMN     "focusKeyword" TEXT,
--- ADD COLUMN     "seoDesc" TEXT,
--- ADD COLUMN     "seoTitle" TEXT,
--- ADD COLUMN     "title" TEXT NOT NULL;
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "authorBioEn",
+DROP COLUMN "authorBioEs",
+DROP COLUMN "contentEn",
+DROP COLUMN "contentEs",
+DROP COLUMN "focusKeywordEn",
+DROP COLUMN "focusKeywordEs",
+DROP COLUMN "seoDescEn",
+DROP COLUMN "seoDescEs",
+DROP COLUMN "seoTitleEn",
+DROP COLUMN "seoTitleEs",
+DROP COLUMN "titleEn",
+DROP COLUMN "titleEs",
+ADD COLUMN     "authorBio" TEXT,
+ADD COLUMN     "content" TEXT NOT NULL,
+ADD COLUMN     "focusKeyword" TEXT,
+ADD COLUMN     "seoDesc" TEXT,
+ADD COLUMN     "seoTitle" TEXT,
+ADD COLUMN     "title" TEXT NOT NULL;
